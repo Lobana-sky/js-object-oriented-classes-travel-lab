606d00bb2c78f7fc049b7f6143bcfad0807f0aeb
lobana-sky <loubanaeng@gmail.com>
2020-02-21 10:27:34 +0300

    Driver

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+class Driver{
+constructor(name,day){
+this.name=name
+this.day=day
+}
+
+static startDate(){
+const st = new Date('1980');
+return st.getFullYear();
+}
+
+ yearsExperienceFromBeginningOf(){
+let d=new Date();
+   console.log(Driver.startDate())
+return d.getFullYear()- Driver.startDate() ;
+}
+}
+
+let s=new Driver()
+s.yearsExperienceFromBeginningOf()
+
