9189a3cfbb9c85eb082363ba76945c06276fb96f
lobana-sky <loubanaeng@gmail.com>
2020-02-21 14:46:45 +0300

    not estimate

--- index.js
+++ index.js
@@ -40,18 +40,28 @@ class Route{
   blocksTravelled(eastWest){
    return Math.abs(this.beginningLocation.vertical-this.endingLocation.vertical)+Math.abs(eastWest.indexOf(this.beginningLocation.horizontal)-eastWest.indexOf(this.endingLocation.horizontal))
   }
-  estimatedTime(){
-    
+  estimatedTime(eastWest,peak){
+    if(peak){
+      return Math.ceil(this.blocksTravelled(eastWest)/3);
+    }
+    else{
+      return Math.ceil(this.blocksTravelled(eastWest)/2);
+    }
   }
 }
 //   let route = new Route(
 //         { horizontal: 'Park', vertical: '34' },
 //         { horizontal: 'Park', vertical: '45' }
 //       );
+// let route = new Route(
+//         { horizontal: '1st Avenue', vertical: '34' },
+//         { horizontal: 'Park', vertical: '30' }
+//       );
 let route = new Route(
         { horizontal: '1st Avenue', vertical: '34' },
-        { horizontal: 'Park', vertical: '30' }
+        { horizontal: 'Park', vertical: '45' }
       );
 console.log(route);
 route.blocksTravelled(eastWest)
-
+let peak=false;
+route.estimatedTime(eastWest,peak)
